979b3258b09ef67c2f75de2ab0eb3c80c5f44a5f
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-12-08 10:54:52 +0330

    change-color-withe-js

---
+++ js.js
@@ -0,0 +1,20 @@
+let liELem = document.querySelectorAll('li')
+
+function red () {
+    liELem[0].style.color = 'red'
+}
+
+function green (){
+    liELem[1].style.color = 'green'
+}
+
+function yellow (){
+    liELem[2].style.color = "yellow"
+}
+
+function pink() {
+    liELem[3].style.color = "pink"
+}
+
+
+console.log(liELem)
